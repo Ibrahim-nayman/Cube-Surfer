ff39b484b2966d385af96bba2ca5afa920645963
İbrahim Nayman <84410079+Ibrahim-nayman@users.noreply.github.com>
2022-01-04 11:36:12 +0300

    Update Prefabs and create different obstacle types

--- Assets/_Project/Scripts/CubeSpawner.cs
+++ Assets/_Project/Scripts/CubeSpawner.cs
@@ -15,7 +15,7 @@ public class CubeSpawner : MonoBehaviour
         {
             Destroy(other.gameObject);
             Vector3 pos = Upper.localPosition;
-            pos.y += 1.2f;
+            pos.y += 1;
             Upper.localPosition = pos;
             var spawnedCube = Instantiate(Cube, CubeSpawnerObject.position, Quaternion.identity);
             spawnedCube.transform.SetParent(Upper);
--- Assets/_Project/Scripts/Obstacle.cs
+++ Assets/_Project/Scripts/Obstacle.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void OnTriggerEnter(Collider other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (other.CompareTag("CollectableCube"))
+        {
+            Destroy(other.gameObject, 0.10f);
+        }
     }
 }
